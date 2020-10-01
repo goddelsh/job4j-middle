8d9d6a4b6497e220d3524a1796cc0b51e3d0f8bd
goddelsh <goddelsh@gmail.com>
2020-10-01 11:48:09 +0500

    3.1. Прерывание блокированной нити. [#372119]

--- chapter_001/src/main/java/concurrent/Wget.java
+++ chapter_001/src/main/java/concurrent/Wget.java
@@ -11,7 +11,7 @@ public class Wget {
                         try {
                             Thread.sleep(1000);
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
                         }
                     }
                 }
