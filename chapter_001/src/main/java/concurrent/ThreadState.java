8241e6b96a2fc71f39132656520cb52b04cb7d82
goddelsh <goddelsh@gmail.com>
2020-10-01 10:52:05 +0500

    1.1. Состояние нити. [#372117]

---
+++ chapter_001/src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,27 @@
+package concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        System.out.println(getThreadNameAndState(first));
+        System.out.println(getThreadNameAndState(second));
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(getThreadNameAndState(first));
+            System.out.println(getThreadNameAndState(second));
+        }
+        System.out.println(getThreadNameAndState(first));
+        System.out.println(getThreadNameAndState(second));
+        System.out.println("work completed");
+    }
+
+    static String getThreadNameAndState(Thread thread) {
+        return thread.getName().concat(" is ").concat(thread.getState().toString());
+    }
+}
