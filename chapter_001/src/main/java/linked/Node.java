d590f6833a823a138b46746291f34b7550a29d17
goddelsh <goddelsh@gmail.com>
2020-10-03 10:41:45 +0500

    3. Immutable объекты [#372125]

---
+++ chapter_001/src/main/java/linked/Node.java
@@ -0,0 +1,22 @@
+package linked;
+
+public class Node<T> {
+    private volatile Node next;
+    private volatile T value;
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
