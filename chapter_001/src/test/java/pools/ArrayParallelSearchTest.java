4716f8bcd3f64aa9f6f48f8c68e0ec566cc14e53
goddelsh <goddelsh@gmail.com>
2020-10-20 10:42:45 +0500

    test renamed & redislocated

--- chapter_001/src/test/java/pools/ArrayParallelSearchTest.java
+++ chapter_001/src/test/java/pools/ArrayParallelSearchTest.java
@@ -40,42 +40,4 @@ public class ArrayParallelSearchTest {
         assertThat(result.get(1), is(3));
     }
 
-
-    @Test
-    public void test() throws ExecutionException, InterruptedException {
-        int[][] array = {
-                {1, 1, 1},
-                {2, 2, 2},
-                {3, 3, 3}
-        };
-
-        RolColSum.Sums[] sums = RolColSum.sum(array);
-
-        assertThat(sums[0].getRowSum(), is(3));
-        assertThat(sums[0].getColSum(), is(6));
-        assertThat(sums[1].getRowSum(), is(6));
-        assertThat(sums[1].getColSum(), is(6));
-        assertThat(sums[2].getRowSum(), is(9));
-        assertThat(sums[2].getColSum(), is(6));
-    }
-
-
-    @Test
-    public void test2() throws ExecutionException, InterruptedException {
-        int n = 1000;
-        int[][] data = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                data[i][j] = 1;
-            }
-        }
-        RolColSum.Sums[] result = RolColSum.asyncSum(data);
-        for (int i = 0; i < n; i++) {
-            if (result[i].getColSum() != n || result[i].getRowSum() != n) {
-                throw new RuntimeException("Wrong solution");
-            }
-        }
-    }
-
-
 }
--- chapter_001/src/test/java/pools/RolColSumTest.java
+++ chapter_001/src/test/java/pools/RolColSumTest.java
@@ -33,4 +33,41 @@ public class RolColSumTest {
         assertThat(sums[2].getRowSum(), is(33));
         assertThat(sums[2].getColSum(), is(21));
     }
+
+
+    @Test
+    public void sumAsyncFull() throws ExecutionException, InterruptedException {
+        int[][] array = {
+                {1, 1, 1},
+                {2, 2, 2},
+                {3, 3, 3}
+        };
+
+        RolColSum.Sums[] sums = RolColSum.sum(array);
+
+        assertThat(sums[0].getRowSum(), is(3));
+        assertThat(sums[0].getColSum(), is(6));
+        assertThat(sums[1].getRowSum(), is(6));
+        assertThat(sums[1].getColSum(), is(6));
+        assertThat(sums[2].getRowSum(), is(9));
+        assertThat(sums[2].getColSum(), is(6));
+    }
+
+
+    @Test
+    public void sumAsyncThousandItr() throws ExecutionException, InterruptedException {
+        int n = 1000;
+        int[][] data = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                data[i][j] = 1;
+            }
+        }
+        RolColSum.Sums[] result = RolColSum.asyncSum(data);
+        for (int i = 0; i < n; i++) {
+            if (result[i].getColSum() != n || result[i].getRowSum() != n) {
+                throw new RuntimeException("Wrong solution");
+            }
+        }
+    }
 }
