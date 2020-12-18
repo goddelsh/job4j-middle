b214f1f8a2a256befa5ba1fc6a53186a67d84ebc
Goddelsh <goddelsh@ynadex.com>
2020-12-18 23:13:47 +0500

    todo task init

---
+++ chapter_003/src/main/java/models/Item.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.util.Date;
+
+public class Item {
+    private int id;
+    private String desc;
+    private Date created;
+    private boolean done;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
