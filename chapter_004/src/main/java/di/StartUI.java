7e95941bddbe6609a03739d6829b7cd7f554a3ed
Goddelsh <Goddelsh@icloud.com>
2021-01-23 20:02:18 +0500

    0. Что такое DI? [#372080]

---
+++ chapter_004/src/main/java/di/StartUI.java
@@ -0,0 +1,27 @@
+package di;
+
+public class StartUI {
+
+    private Store store;
+
+    private ConsoleInput consoleInput;
+
+    public StartUI(Store store, ConsoleInput consoleInput) {
+        this.store = store;
+        this.consoleInput = consoleInput;
+    }
+
+    public void ask() {
+        store.add(consoleInput.askStr("Input string: "));
+    }
+
+    public void add(String value) {
+        store.add(value);
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+}
